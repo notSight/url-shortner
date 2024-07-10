3b8fddf09aaab7f3988e3defa1495f04b9b20993
notsight <omtripathi177@gmail.com>
2024-07-10 19:48:26 +0530

    added server side rendering

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const { connectDB } = require('./conectDB.js')
 const URL = require('./models/url.model.js')
 const urlRoute = require('./routes/url.routes.js')
@@ -11,9 +12,17 @@ app.listen(3000,()=>{
 
 app.use(express.json())
 
+app.set('view engine', 'ejs')
+app.set('views', path.resolve('./views'))
+
+app.get('/test', async (req,res)=>{
+    const allUrls = await URL.find({})
+    res.render('homepage', { urls: allUrls })
+})
+
 app.use('/url', urlRoute)
 
-app.get('/:shortId', async (req,res) => {
+app.get('/url/:shortId', async (req,res) => {
     const shortId = req.params.shortId
     const entry = await URL.findOneAndUpdate({
         shortId
